7c3c59af8001b85ec1d82b89b4ba56a7a1872ce0
danhamraj7 <ddan_7@hotmail.com>
2020-12-02 13:57:08 -0500

    updated folders and completed project 1

--- 1-birthday-reminder/setup/src/App.js
+++ 1-birthday-reminder/setup/src/App.js
@@ -1,8 +1,26 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
+
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data)
+
+  const handleChange = () =>{
+    setPeople([])
+  }
+  
+  return (
+     <main>
+     <section className="container">
+     <h2>{people.length} Birthdays today</h2>
+     <List people={people}/>
+     <button onClick={handleChange}>
+     clear all!
+     </button>
+     </section>
+     </main>
+  ) 
 }
 
 export default App;
