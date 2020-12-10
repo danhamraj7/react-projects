fea199bb9daf0fa911be5ad35096cf200abae384
danhamraj7 <ddan_7@hotmail.com>
2020-12-10 11:12:52 -0500

    updated all files. All features in the app is working.

--- 2-tours/setup/README.md
+++ 2-tours/setup/README.md
@@ -1,1 +1,3 @@
 ## React Projects Starter APP
+
+In this app the user can view all the tours. They can read more by clicking the read more tab. The user can also remove a specific tour by clicking the 'Not Interested' button and reload the tours by clicking the 'reload tours' button.
--- 2-tours/setup/src/App.js
+++ 2-tours/setup/src/App.js
@@ -4,8 +4,60 @@ import Tours from './Tours'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
+
+
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+  
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+    const response = await fetch(url);
+    const tours = await response.json();
+    setLoading(false); 
+    setTours(tours); 
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }  
+  }
+
+  useEffect(() => {
+    fetchTours();
+    
+  }, [])
+
+  if (loading) {
+    return(
+      <main>
+      <Loading/> 
+      </main>
+    )
+  }
+
+  const removeTour = (id) =>{
+    const newTour = tours.filter((tour) => tour.id !== id);
+    setTours(newTour);
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+      <div className="title">
+      <h2>That was the last tour</h2>
+      <button className="btn" onClick={fetchTours}>Reload Tours</button>
+      </div>
+      </main>
+    )
+  }
+
+  return (
+  <main>
+  <Tours tours={tours} removeTour={removeTour}/>
+  </main>
+  )
+  
 }
 
-export default App
+export default App;
